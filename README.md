8d1ecbf77492f02c980935efcec4e9f1842e1acb
Ariel <ariel.tirado@usach.cl>
2020-03-25 03:10:37 -0300

    created http methods for course service

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # TeSigoApp-backend-refractor
+
+## REST Services
+
+### 1) Get the list of courses of an specific user (GET):
+`http://host:por/api/users/{user_id}/courses`
+`http://host:por/api/users/{user_id}/courses?sort=asc`
+`http://host:por/api/users/{user_id}/courses?sort=desc`
+### 2) Get the information of an specific course (GET):
+`http://host:por/api/courses/{course_id}` 
+### 3) Creates a new course and links to a list of users (POST):
+`http://host:por/api/courses/user/{user_id}`
+`http://host:por/api/courses/user/{user_id}?users_ids=id1,id2`
+```javascript
+Request body
+{
+	"name": "4to básico"
+}
+```
--- src/main/java/com/edutools/tesigoapp/TesigoappApplication.java
+++ src/main/java/com/edutools/tesigoapp/TesigoappApplication.java
@@ -1,11 +1,15 @@
 package com.edutools.tesigoapp;
 
-import com.edutools.tesigoapp.Repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan({"com.edutools.tesigoapp", "com.edutools.tesigoapp.rest"})
+@EntityScan("com.edutools.tesigoapp.Entities")
+@EnableJpaRepositories("com.edutools.tesigoapp.repositories")
 public class TesigoappApplication {
 
 	public static void main(String[] args) {
